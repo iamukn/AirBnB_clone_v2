e1c1793ca93b6825c8250df7b50bba30e51d3b81
iamukn <n.u.kingsley@gmail.com>
2023-04-24 21:47:39 +0000

    Hello World in flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
     """This prints Hello HBNB! when the root is accessed
     """
     return ('Hello HBNB!')
-
