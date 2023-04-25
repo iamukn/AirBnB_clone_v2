38989893aa88d3e5480bd6d0fec26bb927a83a24
iamukn <n.u.kingsley@gmail.com>
2023-04-25 00:05:13 +0000

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" Importing flask, creating an instance and
+working with it to serve / and /HBNB page"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """This prints Hello HBNB! when the root is accessed
+    """
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Serves the content of HBNB
+    """
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    split = text.replace('_', ' ')
+    return ("C {0}".format(split))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def double_route(text="is cool"):
+    """serves the /python page and /python/<text>
+    """
+    if text:
+        replace = text.replace('_', ' ')
+        return ("Python {0}".format(replace))
+    else:
+        return ("Python {0}".format(text))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    d = isinstance(n, int)
+    if d:
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
