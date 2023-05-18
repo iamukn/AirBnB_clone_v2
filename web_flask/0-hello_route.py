6ec0e90dc41d1ad998a7264ebd5c3153f18d5b7c
root <n.u.kingsley@gmail.com>
2023-05-18 21:54:09 +0000

    updated the 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,11 +7,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def home():
     """This prints Hello HBNB! when the root is accessed
     """
     return ('Hello HBNB!')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
